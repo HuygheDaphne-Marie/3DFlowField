54aed0490f28b2290f596c4000ad822a0f578602
Henri-Thibault Huyghe <henrithibault.huyghe@gmail.com>
2021-01-12 11:38:18 +0100

    Added initial project code
    Little work already done with adding cell and flowfield scripts and setting up the scene.

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell
+{
+    public Vector3 worldPos;
+    public Vector3Int gridCoordinates;
+
+    public Cell(Vector3 _worldpos, Vector3Int _gridCoords)
+    {
+        worldPos = _worldpos;
+        gridCoordinates = _gridCoords;
+    }
+}
---
+++ Assets/Scripts/FlowField.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowField : MonoBehaviour
+{
+    public Vector3 flowFieldWorldSize;
+    public float cellRadius;
+    
+    Cell[,,] cells;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, flowFieldWorldSize);
+    }
+}
